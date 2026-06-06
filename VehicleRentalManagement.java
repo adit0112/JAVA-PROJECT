5f21de850e4c1994a56d0b26d778ce6bde08200a
adit0112 <theadit0112@gmail.com>
2026-06-06 14:59:28 +0530

    Updated project

--- VehicleRentalManagement.java
+++ VehicleRentalManagement.java
@@ -32,4 +32,50 @@ class Car extends Vehicle {
     public Car(String vehicleNumber, String brand, double rentPerDay) {
         super(vehicleNumber, brand, rentPerDay);
     }
+    // Method Overloading 
+    @Override
+    public double calculateRent(int days) {
+        return rentperDay * days;
+    }
 }
+
+
+// Bike Class
+class Bike extends Vehicle {
+
+    public Bike(String vehicleNumber, String brand, double rentPerDay) {
+        super(vehicleNumber, brand, rentPerDay);
+    }
+
+    // Method Overriding
+    @Override
+    public double calculateRent(int days) {
+        return rentPerDay * days * 0.9; // lower pricing
+    }
+}
+
+// Main Class
+public class VehicleRentalManagement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== VEHICLE RENTAL SYSTEM =====");
+
+        System.out.print("Enter Vehicle Type (car/bike): ");
+        String type = sc.nextLine();
+
+        System.out.print("Enter Vehicle Number: ");
+        String number = sc.nextLine();
+
+        System.out.print("Enter Brand: ");
+        String brand = sc.nextLine();
+
+        System.out.print("Enter Rent per Day: ");
+        double rent = sc.nextDouble();
+
+        System.out.print("Enter Number of Days: ");
+        int days = sc.nextInt();
+
+        Vehicle v;
